1a9159d5308a82f33b3f4e2013d2f5d42c60a30b
QuanNgHa <quanngha.dev@gmail.com>
2020-03-19 23:02:05 +0800

    Classed #3 - Express API

---
+++ dbschema.js
@@ -0,0 +1,12 @@
+let db = {}
+screams: [
+    {
+        userHandle: 'user',
+        body: 'This is the scream body',
+        createdAt: '2020-03-19T14:54:29.657Z',
+        likeCount: 5,
+        commentCount: 2
+
+    }
+]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,50 @@
 const functions = require('firebase-functions');
 
-const admin = require('firebase-admin');
-admin.initializeApp();
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('screams').get().then((data) => {
-        let screams = [];
-        data.forEach((doc) => {
-            screams.push(doc.data());
-        })
-        return res.json(screams)
-    }).catch((err) => console.error(err));
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./keys/serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://socialape-689a2.firebaseio.com"
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(400).json({ error: 'Method is not allowed' })
-    }
+const express = require('express');
+const app = express();
 
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+
+
+app.get('/screams', (req, res) => {
+    admin
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((data) => {
+            let screams = [];
+            data.forEach((doc) => {
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+
+                });
+            })
+            return res.json(screams)
+        }).catch((err) => console.error(err));
+})
+
+
+app.post('/scream', (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     }
 
     admin.firestore().collection('screams').add(newScream)
@@ -30,13 +53,8 @@ exports.createScream = functions.https.onRequest((req, res) => {
         }).catch((err) => {
             res.status(500).json({ error: `something wrong` })
             console.error(err);
-        })
-
-})
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+        });
 });
+
+// https://baseurl.com/api/
+exports.api = functions.region('asia-east2').https.onRequest(app);
