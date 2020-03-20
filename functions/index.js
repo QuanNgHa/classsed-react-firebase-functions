9d1b26dbf1735dc2df5a006220da2480c6fa0e8b
QuanNgHa <quanngha.dev@gmail.com>
2020-03-20 23:46:55 +0800

    Classed #5 Form Validation and Login route

--- functions/index.js
+++ functions/index.js
@@ -68,7 +68,15 @@ app.post('/scream', (req, res) => {
         });
 });
 
-
+const isEmpty = (string) => {
+    if (string.trim() === '') return true;
+    else return false;
+}
+const isEmail = (email) => {
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(emailRegEx)) return true;
+    else return false;
+}
 //Signup route:
 
 app.post('/signup', (req, res) => {
@@ -78,6 +86,22 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle,
     }
+
+    let errors = {};
+
+    //Validation the Data
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty'
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Email must be valid email address'
+    }
+
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
     let token, userId;
 
     //TODO: Validate data
@@ -119,6 +143,40 @@ app.post('/signup', (req, res) => {
 
 })
 
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+
+    }
+    let errors = {};
+
+    //Validation the Data
+    if (isEmpty(user.email)) {
+        errors.email = 'Must not be empty'
+    } else if (!isEmail(user.email)) {
+        errors.email = 'Email must be valid email address'
+    }
+    if (isEmpty(user.password)) errors.password = 'Must not be empty';
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            if (err.code === "auth/wrong-password") {
+                return res.status(403).json({ general: "Wrong credentials, please try again" })
+            } else return res.status(500).json({ error: err.code })
+        })
+
+
+
+
+})
 
 // https://baseurl.com/api/
 exports.api = functions.region('asia-east2').https.onRequest(app);
