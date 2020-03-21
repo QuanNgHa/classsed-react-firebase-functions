43652901325f96525f5a5e4de8d2025ad28f2a31
QuanNgHa <quanngha.dev@gmail.com>
2020-03-21 22:40:09 +0800

    Classed #8-Image Upload

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const app = express();
 const FBAuth = require('./util/fbAuth');
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login } = require('./handlers/users');
+const { signup, login, uploadImage } = require('./handlers/users');
 
 //Scream Route
 app.get('/screams', getAllScreams);
@@ -14,6 +14,7 @@ app.post('/scream', FBAuth, postOneScream);
 //users route
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
 
 // https://baseurl.com/api/
 exports.api = functions.region('asia-east2').https.onRequest(app);
