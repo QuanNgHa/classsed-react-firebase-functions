7615a45423a8485e9dbee7b6addec4b6857bcb76
QuanNgHa <quanngha.dev@gmail.com>
2020-03-22 15:31:16 +0800

    Classed#10 - Getting Comment on Scream

--- dbschema.js
+++ dbschema.js
@@ -20,7 +20,15 @@ let db = {
             commentCount: 2
 
         }
-    ]
+    ],
+    comments: [
+        {
+            userHandle: 'user',
+            screamId: 'kdjsfgdksuufhgkdsufky',
+            body: 'nice one mate!',
+            createdAt: '2019-03-15T10:59:52.798Z'
+        }
+    ],
 }
 
 const userDetails = {
--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,19 @@ const app = express();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 //Scream Route
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
+
+//TODO: delete scream
+//TODO: like a scream
+//TODO: unlike a scream
+//TODO: comment on scream
 
 //users route
 app.post('/signup', signup);
